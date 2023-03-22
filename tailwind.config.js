21ebb07c768bba3609c7a14ccf32664aebfefa7f
Ritesh Rana <13ritesh2001@gmail.com>
2023-03-22 12:32:36 +0530

    adding configuration and scripts

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "*.html"
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
